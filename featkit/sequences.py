6a5a883ecc8ebb8044056401829a4f7c649a50a0
ryad <r.zenine@gmail.com>
2015-06-25 23:11:56 +0100

    Added a SequenceHasher tranformer
    Implemented this transformer in order to handle lists avec gps data.

--- featkit/sequences.py
+++ featkit/sequences.py
@@ -1,16 +1,19 @@
 #! -*- coding:utf-8 -*-
 
 from sklearn.base import TransformerMixin
+from sklearn.feature_extraction import FeatureHasher
 
 
-class SequenceEncodingTransformer(TransformerMixin):
+class SequenceHasher(TransformerMixin):
     """ encodes a sequeces xyz as xy,yz,z"""
-    def __init__(self, padding=1, overlap=False, backward=False):
-        self.padding = padding
-        self.overlap = overlap
+    def __init__(self, base_feature_name="index"):
+        self.base_feature_name = base_feature_name
+        self.hasher = FeatureHasher(input_type="pair")
 
-    def fit(X, y=None):
+    def fit(self, X, y=None):
         pass
 
-    def transform(X, y=None):
-        pass
+    def transform(self, X, y=None):
+        f_name = self.base_feature_name
+        seq = (((f_name + str(i), v) for i, v in enumerate(x)) for x in X)
+        return self.hasher.transform(seq)
